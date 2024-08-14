b0f9e081a41fbf8fe1eabc82e71ffb4d6742e2dc
Nicolas Fernandez Palma <nicofpalmaa@gmail.com>
2024-08-14 17:32:11 -0300

    Working on programs sidebar

--- src/components/taskBar/TaskBar.jsx
+++ src/components/taskBar/TaskBar.jsx
@@ -18,11 +18,50 @@ export default function TaskBar({children}){
             <TimeWidget />
 
             <div className='start-programs-container'>
-                <div className='start-program'>
-                    <img src="src/assets/shutdown.png" alt="" height={"50px"} />
-                    <p>Shut Down...</p>
+                <div className="start-programs-content">
+                    <div className='sidelogo'>
+                        <img src="src/assets/sidelogo.png" alt="" />
+                    </div>
+                    <div className="start-programs-list">
+                        <div className='start-program tool-menu-option'>
+                            <img src="src/assets/shutdown.png" alt="Shutdown" title='Shutdown' height={"35px"}/>
+                            <p>Shut Down...</p>
+                        </div>
+
+                        <div className='tool-menu-line'></div>
+                        
+                        <div className='start-program tool-menu-option'>
+                            <img src="src/assets/run.png" alt="Run" title="Shutdown" height={"35px"}/>
+                            <p>Run...</p>
+                        </div>
+
+                        <div className="start-program tool-menu-option">
+                            <img src="src/assets/help.png" alt="Help" title='Help' height={"30px"}/>
+                            <p>Help</p>
+                        </div>
+
+                        <div className="start-program tool-menu-option">
+                            <img src="src/assets/find.png" alt="" height={"40px"} />
+                            <p>Find</p>
+                        </div>
+
+                        <div className="start-program tool-menu-option">
+                            <img src="src/assets/settings.png" alt="" />
+                            <p>Settings</p>
+                        </div>
+
+                        <div className='start-program tool-menu-option'>
+                            <img src="src/assets/documents.png" alt="" />
+                            <p>Documents</p>
+                        </div>
+
+                        <div className='start-program tool-menu-option'>
+                            <img src="src/assets/programs.png" alt="" />
+                            <p>Programs</p>
+                        </div>
+                    </div>
+
                 </div>
-                <div className='tool-menu-line'></div>
             </div>
         </footer>
     )
